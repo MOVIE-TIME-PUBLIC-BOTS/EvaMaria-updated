08373229289519afa6a5bff1bb8762865a1906d4
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-08-19 11:50:01 +0530

    Update arrow.py

--- plugins/fun/arrow.py
+++ plugins/fun/arrow.py
@@ -14,9 +14,9 @@ DART_E_MOJI = "🎯"
 )
 async def throw_dart(client, message):
     """ /throw an @AnimatedDart """
-    rep_mesg_id = message.message_id
+    rep_mesg_id = message.id
     if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.message_id
+        rep_mesg_id = message.reply_to_message.id
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=DART_E_MOJI,
