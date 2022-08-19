33eebe957e58f856e5af0c9a621e85f30a20c57e
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-08-19 11:49:41 +0530

    Update dice.py

--- plugins/fun/dice.py
+++ plugins/fun/dice.py
@@ -14,9 +14,9 @@ DICE_E_MOJI = "🎲"
 )
 async def roll_dice(client, message):
     """ @RollaDie """
-    rep_mesg_id = message.message_id
+    rep_mesg_id = message.id
     if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.message_id
+        rep_mesg_id = message.reply_to_message.id
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=DICE_E_MOJI,
