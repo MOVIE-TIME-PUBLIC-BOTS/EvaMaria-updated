b116640bb10e008b9873ef22f7d2450098e18959
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-08-19 11:50:37 +0530

    Update goal.py

--- plugins/fun/goal.py
+++ plugins/fun/goal.py
@@ -13,9 +13,9 @@ GOAL_E_MOJI = "⚽"
 )
 async def roll_dice(client, message):
     """ @Goal """
-    rep_mesg_id = message.message_id
+    rep_mesg_id = message.id
     if message.reply_to_message:
-        rep_mesg_id = message.reply_to_message.message_id
+        rep_mesg_id = message.reply_to_message.id
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=GOAL_E_MOJI,
